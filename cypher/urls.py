2a54dce28b7cc4748a11e4076bbcd15c07fc363a
KevinIsAProfessional <k.j.christensen93@gmail.com>
2020-10-21 22:21:05 -0600

    removed cyphertemp.html, cypher and caesar both have their own pages now

--- cypher/urls.py
+++ cypher/urls.py
@@ -1,7 +1,8 @@
 # cypher/urls.py
 from django.urls import path
-from .views import CypherPageView
+from .views import CypherPageView, CaesarPageView
 
 urlpatterns = [
     path('cypher/', CypherPageView.as_view(), name='cypher'),
+    path('caesar/', CaesarPageView.as_view(), name='caesar')
 ]
--- cypher/views.py
+++ cypher/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class CypherPageView(TemplateView):
     template_name = 'cypher/cypher.html'
+
+class CaesarPageView(TemplateView):
+    template_name = 'cypher/caesar.html'
