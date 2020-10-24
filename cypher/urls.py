003506e7841468856dfebfadc1e496c4f6425850
KevinIsAProfessional <k.j.christensen93@gmail.com>
2020-10-24 00:39:32 -0600

    just kidding. NOW all of the links are working

--- cypher/urls.py
+++ cypher/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('cypher/', CypherPageView.as_view(), name='cypher'),
     path('caesar/', CaesarPageView.as_view(), name='caesar'),
     path('sha/', ShaPageView.as_view(), name='sha'),
-    path('other/', OtherPageView.as_view, name='other'),
+    path('other/', OtherPageView.as_view(), name='other'),
 ]
