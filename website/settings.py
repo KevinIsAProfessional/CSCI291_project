eedd0f537f9235c7a9517cd0d115a96f34ebae1c
KevinIsAProfessional <k.j.christensen93@gmail.com>
2020-12-09 17:15:17 -0700

    got rid of login app, cuz dont need'

--- website/settings.py
+++ website/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
     'cypher.apps.CypherConfig',
-    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,8 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-AUTH_USER_MODEL = 'login.CustomUser'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
